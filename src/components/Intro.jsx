cff88644fb4f40b44d9f0e0854f1477b47bdc60e
Haseeb98-Git <hasseebalioriginal@gmail.com>
2025-03-15 23:30:17 +0500

    frontend brightness and some cosmetic changes, added chat messages functionality in create floorplan screen

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -19,8 +19,8 @@ const Intro = () =>{
         {/* Main Intro Heading: Design with your voice with VoiceArchi */}
         <img src={voicearchi_svg_intro_title} alt="VoiceArchi Intro Heading" className="h-20 w-auto absolute left-1/2 transform -translate-x-1/2 top-35" />
         {/* Floorplan SVGs */}
-        <img src={voicearchi_svg_floorplan_outer_wall} alt="Floorplan SVG" className="animate-book-open-right invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
-        <img src={voicearchi_svg_floorplan_inner_wall} alt="Floorplan SVG" className="animate-book-open-left invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
+        <img src={voicearchi_svg_floorplan_outer_wall} alt="Floorplan SVG" className="animate-scroll-open invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
+        <img src={voicearchi_svg_floorplan_inner_wall} alt="Floorplan SVG" className="animate-reveal invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
         <img src={voicearchi_svg_floorplan_sofas} alt="Floorplan SVG" className="animate-reveal invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
         <img src={voicearchi_svg_floorplan_diningtable} alt="Floorplan SVG" className="animate-reveal-reverse invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
         <img src={voicearchi_svg_floorplan_diningtable2} alt="Floorplan SVG" className="animate-reveal invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
@@ -31,7 +31,7 @@ const Intro = () =>{
         <img src={voicearchi_svg_floorplan_bathitems} alt="Floorplan SVG" className="animate-reveal invert-100 h-50 w-auto absolute left-1/2 transform -translate-x-1/2 top-70" />
 
       {/* Main Intro description */}
-      <h1 className = "text-gray-200 font-montserrat w-150 h-auto absolute left-1/2 transform -translate-x-1/2 top-140">VoiceArchi transforms your voice commands into 2D floorplans instantly. Simply describe your idea, and our AI-powered system will convert it into a structured design, streamlining the floorplan creation process effortlessly.</h1>
+      <h1 className = "text-gray-100 font-montserrat w-150 h-auto absolute left-1/2 transform -translate-x-1/2 top-140">VoiceArchi transforms your voice commands into 2D floorplans instantly. Simply describe your idea, and our AI-powered system will convert it into a structured design, streamlining the floorplan creation process effortlessly.</h1>
       {/* Get Started Now Button */}
       <button className="hidden absolute left-1/2 transform -translate-x-1/2 top-170 md:block border border-voicearchi_purple_bright text-white px-4 py-1 mb-3 rounded-lg font-montserrat text-xl font-bold hover:bg-voicearchi_purple_glow_dim"> Get Started</button>
       </div>
